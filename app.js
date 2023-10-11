d39d9ddd0c6c37333e995d8adcfa2921e345b2dd
Njaaga-Gakure <50949648+Njaaga-Gakure@users.noreply.github.com>
2023-10-11 14:17:56 +0300

    Create a home route

--- app.js
+++ app.js
@@ -29,6 +29,9 @@ app.use(helmet());
 app.use(cors());
 app.use(xss());
 
+app.get("/", (req, res) => {
+  res.status(200).send("Re.Ex api");
+});
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/financials", authenticateUser, financialRouter);
 app.use(notFound);
